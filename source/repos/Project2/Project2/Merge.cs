3dc156f5e0a17c746650abcc7d2f3484b22eb110
Sofia <sk251299@outlook.com>
2023-06-13 17:56:55 +0300

    v1

---
+++ source/repos/Project2/Project2/Merge.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Project2
+{
+    internal class Solution1
+    {
+        public static void Merge (int[] nums1, int m, int [] nums2, int n)
+        {
+            int index1 = m - 1; // Index of the last element in nums1
+            int index2 = n - 1; // Index of the last element in nums2
+            int mergetIndex = m + n - 1; // The index of the last element in the combined array nums1
+
+            while (index1 >=0 && index2 >=0) // Merging arrays in ascending order
+            {
+                if (nums1[index1] < nums2[index2])
+                {
+                    nums1[mergetIndex] = nums2[index2];
+                    index2--;
+                }
+                else
+                {
+                    nums1[mergetIndex] = nums1[index1];
+                    index1--;
+                }
+                mergetIndex--;
+            }
+            // Adding the remainders from nums2 in case the elements of nums1 are already sorted
+            while (index2>=0)
+            {
+                nums1[mergetIndex] = nums2[index2];
+                index2--;
+                mergetIndex--;
+            }
+        }
+        static void Main (string [] args)
+        {
+            Console.WriteLine("Enter the elements of the nums1 array, separated by a space:");
+            string nums1Input = Console.ReadLine();
+            int[] nums1 = Array.ConvertAll(nums1Input.Split(','), int.Parse);
+
+            Console.WriteLine("Enter the elements of the nums2 array, separated by a space:");
+            string nums2Input = Console.ReadLine();
+            int[] nums2 = Array.ConvertAll(nums2Input.Split(','), int.Parse);
+
+            Console.WriteLine("Enter the number of elements in nums1:");
+            int m = int.Parse (Console.ReadLine());
+
+            Console.WriteLine("Enter the number of elements in nums2:");
+            int n = int.Parse(Console.ReadLine());
+
+            Merge(nums1, m, nums2, n); // Call function to merge and sort arrays
+
+            Console.WriteLine("Result: "); // Output the result
+
+            foreach (int num in nums1)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/repos/Project2/Project2/Two sum.cs
@@ -0,0 +1,86 @@
+﻿/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.*/
+
+using System;
+
+
+namespace Project2
+{
+    public class Solution
+    {
+        static void BubleSort(int[] nums, int[] indices)
+        {
+            int size = nums.Length;
+            for (int i = 1; i < size; i++)
+            {
+                for (int j = 0; j < size - i; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        //Exchange of elements
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+
+                        //Index exchange
+                        int tempIndex = indices[j];
+                        indices[j] = indices [j+1];
+                        indices [j+1] = tempIndex;
+                    }
+                }
+            }
+
+        }
+        static int[] TwoSum(int[] nums, int target)
+        {
+            int l = 0;
+            int r = nums.Length - 1;
+
+            int[] sortedIndices = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++) // preserve the original indexes before sorting
+            {
+                sortedIndices[i] = i;
+            }
+            
+            BubleSort (nums, sortedIndices); // sort nums array and corresponding indexes
+
+            while (l < r)
+            {
+                int sum = nums[l] + nums[r];
+
+                if (sum == target) //if the sum is equal to the number, we return the indices of the given elements
+                {
+                    int index1 = sortedIndices[l];  
+                    int index2 = sortedIndices[r];
+
+                    return new int[]{index1, index2};
+                  
+                }
+                if (sum < target)
+                {
+                    l++; //if the sum is less than the number, move to the right
+                }
+                else
+                {
+                    r--; //if the sum is greater than the number, move to the left
+                }
+
+            }
+            return new int[0]; // return an empty array if no pair of elements with this sum is found
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine(); //enter array elements and a number
+            int[] nums = Array.ConvertAll(input.Split(','), int.Parse);
+            int target = int.Parse(Console.ReadLine());
+
+            int[] result = TwoSum(nums, target); // we derive the indices of the elements
+            Console.WriteLine(string.Join(", ", result));
+
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+   
+}
